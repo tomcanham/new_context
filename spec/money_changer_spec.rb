15c188dd516b6994f07beca6f8d7a0b640d61dcc
Tom Canham <tom@arcadianproductions.com>
2015-09-29 12:10:32 -0700

    Added test to validate that a zero initial value works as expected

--- spec/money_changer_spec.rb
+++ spec/money_changer_spec.rb
@@ -4,8 +4,26 @@ require 'pry'
 include MoneyChanger
 
 RSpec.describe Changer do
-  it "correctly calculates denominations down to cents" do
-      changer = Changer.new(999.99)
+  context "denomination enumeration" do
+    it "correctly calculates denominations down to cents" do
+        changer = Changer.new(999.99)
+
+        denominations, string_result = changer.get_denominations
+        expect(denominations).to eq({
+          10000 => 9,
+          5000 => 1,
+          2000 => 2,
+          500 => 1,
+          200 => 2,
+          50 => 1,
+          25 => 1,
+          10 => 2,
+          1 => 4
+          })
+    end
+
+    it "stops when the balance is zero" do
+      changer = Changer.new(999.95)
 
       denominations, string_result = changer.get_denominations
       expect(denominations).to eq({
@@ -17,35 +35,27 @@ RSpec.describe Changer do
         50 => 1,
         25 => 1,
         10 => 2,
-        1 => 4
         })
+    end
+
+    it "produces the correct textual results" do
+      changer = Changer.new(999.99)
+
+      denominations, string_result = changer.get_denominations
+      expect(string_result).to eq("9 hundred dollar bills, 1 fifty dollar bill, 2 twenty dollar bills, " +
+      "1 five dollar bill, 2 two dollar bills, 1 fifty cent piece, 1 quarter, 2 dimes, 4 pennies")
+    end
+
+    it "handles zero correctly" do
+      changer = Changer.new(0)
+
+      denominations, string_result = changer.get_denominations
+      expect(denominations).to be_empty
+      expect(string_result).to be_empty
+    end
   end
 
-  it "stops when the balance is zero" do
-    changer = Changer.new(999.95)
-
-    denominations, string_result = changer.get_denominations
-    expect(denominations).to eq({
-      10000 => 9,
-      5000 => 1,
-      2000 => 2,
-      500 => 1,
-      200 => 2,
-      50 => 1,
-      25 => 1,
-      10 => 2,
-      })
-  end
-
-  it "produces the correct textual results" do
-    changer = Changer.new(999.99)
-
-    denominations, string_result = changer.get_denominations
-    expect(string_result).to eq("9 hundred dollar bills, 1 fifty dollar bill, 2 twenty dollar bills, " +
-    "1 five dollar bill, 2 two dollar bills, 1 fifty cent piece, 1 quarter, 2 dimes, 4 pennies")
-  end
-
-  context "pluralization" do
+  context "pluralization helper" do
     it "pluralizes pennies correctly" do
       changer = Changer.new(0.04)
 
